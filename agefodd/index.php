ffe66f9b523f7b1c53a456bdf98681624426db0c
Regis Houssin <regis@dolibarr.fr>
2011-03-20 19:20:21 +0000

    Fix: external modules can use an alternative path in 3.0

--- agefodd/index.php
+++ agefodd/index.php
@@ -1,9 +1,6 @@
 <?php
-/* Copyright (C) 2003		Rodolphe Quiedeville	<rodolphe@quiedeville.org>
- * Copyright (C) 2004-2008	Laurent Destailleur		<eldy@users.sourceforge.net>
- * Copyright (C) 2004		Eric Seigne				<eric.seigne@ryxeo.com>
- * Copyright (C) 2005-2011	Regis Houssin			<regis@dolibarr.fr>
- * Copyright (C) 2009-2010	Erick Bullier			<eb.dev@ebiconsulting.fr>
+/* Copyright (C) 2009-2010	Erick Bullier	<eb.dev@ebiconsulting.fr>
+ * Copyright (C) 2010-2011	Regis Houssin	<regis@dolibarr.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
