a01b8b6cbe39115873753cf16fbca3f29506f5d2
Regis Houssin <regis@dolibarr.fr>
2011-10-24 19:11:51 +0200

    compatibility with 3.2

--- core/modules/modAgefodd.class.php
+++ core/modules/modAgefodd.class.php
@@ -110,7 +110,7 @@ class modAgefodd extends DolibarrModules
 		$this->boxes = array();			// List of boxes
 		$r=0;
 
-		// Add here list of php file(s) stored in includes/boxes that contains class to show a box.
+		// Add here list of php file(s) stored in core/boxes that contains class to show a box.
 		// Example:
 		//$this->boxes[$r][1] = "myboxa.php";
 		//$r++;
