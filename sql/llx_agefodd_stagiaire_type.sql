c0243790f0f3434140ab00443cbf892a9b267e55
FHenry <florian.henry@open-concept.pro>
2012-06-25 15:25:43 +0200

    add stagiaire type

--- sql/llx_agefodd_stagiaire_type.sql
+++ sql/llx_agefodd_stagiaire_type.sql
@@ -38,9 +38,12 @@ CREATE TABLE IF NOT EXISTS llx_agefodd_stagiaire_type (
 -- Contenu de la table llx_agefodd_stagiaire_type
 --
 
-INSERT INTO llx_agefodd_stagiaire_type (rowid, intitule, sort, datec, tms, fk_user_author, fk_user_mod) VALUES
-(1, 'financement par l''employeur (contrat pro.)', 1, '0000-00-00 00:00:00', '2010-06-30 18:47:43', 0, 0),
-(2, 'financement par l''employeur (autre)', 2, '0000-00-00 00:00:00', '2010-06-30 18:47:56', 0, 0),
-(3, 'demandeur d''emploi avec financement public', 3, '0000-00-00 00:00:00', '2010-06-30 18:48:05', 0, 0),
-(4, 'autre', 4, '0000-00-00 00:00:00', '2010-06-30 18:48:11', 0, 0);
+INSERT INTO llx_agefodd_stagiaire_type (intitule, sort, datec, tms, fk_user_author, fk_user_mod) VALUES
+('DIF', 1, '0000-00-00 00:00:00', '2010-06-30 18:48:05', 0, 0),
+('Période PRO', 1, '0000-00-00 00:00:00', '2010-06-30 18:48:05', 0, 0),
+('financement par l''employeur (contrat pro.)', 3, '0000-00-00 00:00:00', '2010-06-30 18:47:43', 0, 0),
+('financement par l''employeur (autre)', 4, '0000-00-00 00:00:00', '2010-06-30 18:47:56', 0, 0),
+('demandeur d''emploi avec financement public', 5, '0000-00-00 00:00:00', '2010-06-30 18:48:05', 0, 0),
+('autre', 6, '0000-00-00 00:00:00', '2010-06-30 18:48:11', 0, 0);
+
 
