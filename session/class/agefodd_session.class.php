5f7f7ec78f4ee47007db4f695837d6deb884cdf1
jfefe <jfefe@aternatik.fr>
2012-08-20 21:22:01 +0200

    Affichage de la couleur en arrière plan de la réf dans les listes de sessions

--- session/class/agefodd_session.class.php
+++ session/class/agefodd_session.class.php
@@ -79,12 +79,13 @@ class Agefodd_session extends CommonObject
     var $contactid;
     var $contactname;
     var $sourcecontactid;
+    var $fk_actioncomm;
 
     /**
      *	\brief		Constructor
      *	\param		DB	Database handler
      */
-    function Agefodd_session($DB)
+    function __construct($DB)
     {
         $this->db = $DB;
         return 1;
@@ -139,9 +140,9 @@ class Agefodd_session extends CommonObject
     	$sql.= " ".(! isset($this->dated) || dol_strlen($this->dated)==0?'NULL':$this->db->idate($this->dated)).",";
     	$sql.= " ".(! isset($this->datef) || dol_strlen($this->datef)==0?'NULL':$this->db->idate($this->datef)).",";
     	$sql.= " ".(! isset($this->notes)?'NULL':"'".$this->db->escape($this->notes)."'").",";
-    	$sql.= " ".$this->db->escape($user).",";
+    	$sql.= " ".$this->db->escape($user->id).",";
     	$sql.= " ".$this->db->idate(dol_now()).",";
-    	$sql.= " ".$this->db->escape($user);
+    	$sql.= " ".$this->db->escape($user->id);
 
     	$sql.= ")";
 
@@ -159,18 +160,19 @@ class Agefodd_session extends CommonObject
     		//Create or update line in session commercial table and get line number
     		if (!empty($this->commercialid))
     		{
-    			$result = $this->setCommercialSession($this->commercialid,$user);
+    			$result = $this->setCommercialSession($this->commercialid,$user->id);
     			if ($result <= 0){
     				$error++; $this->errors[]="Error ".$this->db->lasterror();
     			}
     		}
     		if (!empty($this->contactid))
     		{
-    			$result = $this->setContactSession($this->contactid,$user);
+    			$result = $this->setContactSession($this->contactid,$user->id);
     			if ($result <= 0){
     				$error++; $this->errors[]="Error ".$this->db->lasterror();
     			}
     		}
+
     		if (! $notrigger)
     		{
     			// Uncomment this and change MYOBJECT to your own tag if you
@@ -369,7 +371,6 @@ class Agefodd_session extends CommonObject
     			$this->id    = $obj->rowid;
     			$this->ref    = $obj->rowid; // Use for next prev ref
     			$this->fk_soc    = $obj->fk_soc;
-
     			$this->fk_formation_catalogue = $obj->fk_formation_catalogue;
     			$this->formintitule = $obj->formintitule;
     			$this->formid = $obj->formid;
@@ -653,7 +654,6 @@ class Agefodd_session extends CommonObject
 			}
 		}
 
-
 		if ($error==0)
 		{
 			// Check parameters
@@ -693,6 +693,7 @@ class Agefodd_session extends CommonObject
 
 
 	        $sql.= " WHERE rowid=".$this->id;
+
 			$this->db->begin();
 
 			dol_syslog(get_class($this)."::update sql=".$sql, LOG_DEBUG);
@@ -1218,7 +1219,7 @@ class Agefodd_session extends CommonObject
 	{
 		global $langs;
 
-		$sql = "SELECT s.rowid, s.fk_session_place, s.type_session, s.dated, s.datef, s.is_date_res_site, s.is_date_res_trainer, s.date_res_trainer, ";
+		$sql = "SELECT s.rowid, s.fk_session_place, s.type_session, s.dated, s.datef, s.is_date_res_site, s.is_date_res_trainer, s.date_res_trainer, s.color, s.force_nb_stagiaire, s.nb_stagiaire,";
 		$sql.= " c.intitule, c.ref,";
 		$sql.= " p.ref_interne,";
 		$sql.= " (SELECT count(*) FROM ".MAIN_DB_PREFIX."agefodd_session_stagiaire WHERE fk_session_agefodd=s.rowid) as num";
@@ -1265,6 +1266,7 @@ class Agefodd_session extends CommonObject
 
 		dol_syslog(get_class($this)."::fetch_all sql=".$sql, LOG_DEBUG);
 		$resql=$this->db->query($sql);
+
 		if ($resql)
 		{
 			$this->line = array();
@@ -1288,6 +1290,9 @@ class Agefodd_session extends CommonObject
 					$this->line[$i]->ref = $obj->ref;
 					$this->line[$i]->ref_interne = $obj->ref_interne;
 					$this->line[$i]->num = $obj->num;
+					$this->line[$i]->color = $obj->color;
+					$this->line[$i]->nb_stagiaire = $obj->nb_stagiaire;
+					$this->line[$i]->force_nb_stagiaire = $obj->force_nb_stagiaire;
 
 					$i++;
 				}
