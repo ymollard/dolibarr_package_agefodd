1e6da9af7082fb076c6e0e8a41db4a6b0a756cd2
fhenry <florian.henry@open-concept.pro>
2012-11-15 18:28:11 +0100

    update mig fom 2.9 to 3.2

--- sql/update_2.0.16-2.0.17.sql
+++ sql/update_2.0.16-2.0.17.sql
@@ -1,4 +1,4 @@
-ALTER TABLE llx_agefodd_formation_catalogue ADD COLUMN entity int(11) NOT NULL DEFAULT 1 AFTER ref;
+ALTER TABLE llx_agefodd_formation_catalogue ADD COLUMN entity int(11) NOT NULL DEFAULT 1 AFTER ref_interne;
 ALTER TABLE llx_agefodd_session ADD COLUMN entity int(11) NOT NULL DEFAULT 1 AFTER rowid;
 ALTER TABLE llx_agefodd_stagiaire ADD COLUMN entity int(11) NOT NULL DEFAULT 1 AFTER rowid;
 ALTER TABLE llx_agefodd_place ADD COLUMN entity int(11) NOT NULL DEFAULT 1 AFTER rowid;
