3d526ee1137fb311048c84e91f3848aed18f93dd
fhenry <florian.henry@open-concept.pro>
2013-04-24 15:03:14 +0200

    Fix update migration request

--- sql/update_2.0.22-2.0.29.sql
+++ sql/update_2.0.22-2.0.29.sql
@@ -1,23 +1,10 @@
 --for pgsql
-UPDATE llx_agefodd_stagiaire as st SET
-nom=sp.name,
-prenom=sp.firstname,
-civilite=sp.civilite,
-fk_soc=sp.fk_soc,
-tel1=sp.phone,
-tel2=sp.phone_perso,
-mail=sp.email
-FROM llx_socpeople as sp WHERE st.fk_socpeople=sp.rowid
+UPDATE llx_agefodd_stagiaire as st SET nom=sp.name, prenom=sp.firstname, civilite=sp.civilite, fk_soc=sp.fk_soc, tel1=sp.phone, 
+tel2=sp.phone_perso, mail=sp.email FROM llx_socpeople as sp WHERE st.fk_socpeople=sp.rowid;
 
 --for mysql
-UPDATE llx_agefodd_stagiaire as st, llx_socpeople as sp SET
-nom=sp.name,
-prenom=sp.firstname,
-civilite=sp.civilite,
-fk_soc=sp.fk_soc,
-tel1=sp.phone,
-tel2=sp.phone_perso,
-mail=sp.email
-WHERE st.fk_socpeople=sp.rowid
+UPDATE llx_agefodd_stagiaire as st, llx_socpeople as sp SET nom=sp.name,
+prenom=sp.firstname, civilite=sp.civilite, fk_soc=sp.fk_soc,tel1=sp.phone,
+tel2=sp.phone_perso, mail=sp.email WHERE st.fk_socpeople=sp.rowid;
 
 
