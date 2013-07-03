8735dab9a522e02fb7212ff0baf893dae6043c4b
Gebruiker <Gebruiker@Sipko>
2013-07-03 18:05:09 +0200

    i,portexport certif and trainning

--- sql/update_2.0.30-2.1.0.sql
+++ sql/update_2.0.30-2.1.0.sql
@@ -1,3 +1,6 @@
 ALTER TABLE llx_agefodd_stagiaire ADD COLUMN import_key	varchar(14);
+ALTER TABLE llx_agefodd_stagiaire_certif ADD COLUMN import_key	varchar(14);
+ALTER TABLE llx_agefodd_session_stagiaire ADD COLUMN import_key	varchar(14);
+ALTER TABLE llx_agefodd_stagiaire ADD COLUMN import_key	varchar(14);
 ALTER TABLE llx_agefodd_stagiaire ADD COLUMN date_birth datetime default NULL AFTER mail;
 ALTER TABLE llx_agefodd_stagiaire ADD COLUMN place_birth  varchar(100) default NULL AFTER date_birth;
