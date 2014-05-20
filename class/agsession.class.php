f85f16ed408f3f2356f1327210246ac75afdaea3
Florian HENRY <florian.henry@open-concept.pro>
2014-05-20 15:58:09 +0200

    Fix bugs_agefodd #1402 - Impossible d'ajouter un participant sur une session donnée

--- class/agsession.class.php
+++ class/agsession.class.php
@@ -1987,7 +1987,7 @@ class Agsession extends CommonObject {
 					$sql .= ' AND s.dated>= DATE_ADD(NOW(), INTERVAL -' . $intervalday . ')';
 				} elseif (strpos($key, 'date')) { // To allow $filter['YEAR(s.dated)']=>$year
 					$sql .= ' AND ' . $key . ' = \'' . $value . '\'';
-				} elseif (($key == 's.fk_session_place') || ($key == 'f.rowid') || ($key == 's.type_session') || ($key == 's.status') || ($key == 'sale.fk_user_com')) {
+				} elseif (($key == 's.fk_session_place') || ($key == 'f.rowid') || ($key == 's.type_session') || ($key == 's.status') || ($key == 'sale.fk_user_com') || ($key == 's.rowid')) {
 					$sql .= ' AND ' . $key . ' = ' . $value;
 				} elseif ($key == '!s.status') {
 					$sql .= ' AND s.status <> ' . $value;
