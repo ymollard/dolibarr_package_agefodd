822d924216814498ffbe105139f23d2d45a73844
Florian HENRY <florian.henry@open-concept.pro>
2015-09-28 13:46:29 +0200

    add PDF doc

--- session/document.php
+++ session/document.php
@@ -531,6 +531,7 @@ if (! empty($id)) {
 		document_line($langs->trans("AgfFichePresenceTraineeLandscape"), "fiche_presence_landscape");
 		document_line($langs->trans("AgfFicheEval"), "fiche_evaluation");
 		document_line($langs->trans("AgfRemiseEval"), "fiche_remise_eval");
+		document_line($langs->trans("AgfAttestationEndTrainingEmpty"), "attestationendtraining_empty");
 		
 		print '</table>' . "\n";
 		
