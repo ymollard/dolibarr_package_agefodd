9affb936c67f4ac08f15c9e2dfe504157f27b870
florian HENRY <florian.henry@atm-consulting.fr>
2017-03-03 12:04:28 +0100

    fix OPCA

--- core/triggers/interface_99_modAgefodd_Agefodd.class.php
+++ core/triggers/interface_99_modAgefodd_Agefodd.class.php
@@ -741,6 +741,7 @@ class InterfaceAgefodd {
 						if ($conf->global->AGF_ADD_TRAINEE_NAME_INTO_DOCPROPODR) {
 							$desc_trainee = "\n";
 							$nbtrainee = 0;
+							$num_OPCA_file_array=array();
 							foreach ( $session_trainee->lines as $line ) {
 
 								// Do not output not present or cancelled trainee
@@ -749,7 +750,10 @@ class InterfaceAgefodd {
 										$sessionOPCA->getOpcaForTraineeInSession($line->socid, $this->id,$line->stagerowid);
 									}
 									if (! empty($sessionOPCA->num_OPCA_file)) {
-										$desc_OPCA = "\n" . $langs->trans('AgfNumDossier') . ' : ' . $sessionOPCA->num_OPCA_file . ' ' . $langs->trans('AgfInTheNameOf') . ' ' . $line->socname;
+										if (!array_key_exists($sessionOPCA->num_OPCA_file, $num_OPCA_file_array)) {
+											$desc_OPCA .= "\n" . $langs->trans('AgfNumDossier') . ' : ' . $sessionOPCA->num_OPCA_file . ' ' . $langs->trans('AgfInTheNameOf') . ' ' . $line->socname;
+											$num_OPCA_file_array[$sessionOPCA->num_OPCA_file]=$line->socname;
+										}
 									}
 									$desc_trainee .= dol_strtoupper($line->nom) . ' ' . $line->prenom . "\n";
 									$nbtrainee ++;
