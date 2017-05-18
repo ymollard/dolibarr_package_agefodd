84fc5100bb22eef387d11e065bfb230c0046bd66
florian HENRY <florian.henry@atm-consulting.fr>
2017-05-18 10:16:06 +0200

    FIx document

--- core/modules/agefodd/pdf/pdf_attestationendtraining.modules.php
+++ core/modules/agefodd/pdf/pdf_attestationendtraining.modules.php
@@ -84,6 +84,9 @@ class pdf_attestationendtraining extends ModelePDFAgefodd {
 		$this->colorfooter = agf_hex2rgb($conf->global->AGF_FOOT_COLOR);
 		$this->colortext = agf_hex2rgb($conf->global->AGF_TEXT_COLOR);
 		$this->colorhead = agf_hex2rgb($conf->global->AGF_HEAD_COLOR);
+		$this->colorheaderBg = agf_hex2rgb($conf->global->AGF_HEADER_COLOR_BG);
+		$this->colorheaderText = agf_hex2rgb($conf->global->AGF_HEADER_COLOR_TEXT);
+		$this->colorLine = agf_hex2rgb($conf->global->AGF_COLOR_LINE);
 
 		// Get source company
 		$this->emetteur = $mysoc;
@@ -469,7 +472,7 @@ class pdf_attestationendtraining extends ModelePDFAgefodd {
 		global $conf, $langs;
 
 		$outputlangs->load("main");
-		
+
 		// Fill header with background color
 		$pdf->SetFillColor($this->colorheaderBg[0], $this->colorheaderBg[1], $this->colorheaderBg[2]);
 		$pdf->MultiCell($this->page_largeur, 40, '', 0, 'L', true, 1, 0, 0);
