07635b58799de377e869d5ec51ca0ac5117a5486
gauthier <gauthier.verdol@atm-consulting.fr>
2017-08-11 10:19:55 +0200

    onglet document participant

--- lib/agefodd_document.lib.php
+++ lib/agefodd_document.lib.php
@@ -990,8 +990,8 @@ function getSelectAgefoddModels($mdle, $socid=0) {
 	
 	$type='';
 	if($mdle === 'fiche_evaluation') $type = 'rfltr_agefodd_evaluation'; // TODO gérer tous les cas
-	elseif($mdle === 'attestation') $type = 'rfltr_agefodd_attestation';
-	elseif($mdle === 'attestationendtraining') $type = 'rfltr_agefodd_attestation_fin_formation';
+	elseif($mdle === 'attestation' || $mdle === 'attestation_trainee') $type = 'rfltr_agefodd_attestation';
+	elseif($mdle === 'attestationendtraining' || $mdle === 'attestationendtraining_trainee') $type = 'rfltr_agefodd_attestation_fin_formation';
 	elseif($mdle === 'fiche_presence') $type = 'rfltr_agefodd_presence';
 	
 	$TModels = RfltrTools::getAgefoddModelList();
