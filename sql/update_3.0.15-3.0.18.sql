3ad2a05414b4af6ddf0901d1fedc9319f555e3ab
florian HENRY <florian.henry@atm-consulting.fr>
2017-10-10 17:17:18 +0200

    add fileds

---
+++ sql/update_3.0.15-3.0.18.sql
@@ -0,0 +1,2 @@
+ALTER TABLE llx_agefodd_training_admlevel ADD COLUMN delais_alerte_end integer AFTER delais_alerte;
+ALTER TABLE llx_agefodd_session_admlevel ADD COLUMN delais_alerte_end integer AFTER delais_alerte;
