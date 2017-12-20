2dbbbf488340088d29bb112249361a19d71b6b81
florian HENRY <florian.henry@atm-consulting.fr>
2017-12-20 11:06:40 +0100

    fix nb rows on session list

--- training/list.php
+++ training/list.php
@@ -199,6 +199,11 @@ foreach ($search_array_options as $key => $val)
 	}
 }
 
+$nbtotalofrecords = 0;
+if (empty($conf->global->MAIN_DISABLE_FULL_SCANLIST)) {
+	$nbtotalofrecords = $agf->fetch_all($sortorder, $sortfield, 0, 0, $arch, $filter, array_keys($extrafields->attribute_label));
+}
+
 $resql = $agf->fetch_all($sortorder, $sortfield, $limit, $offset, $arch, $filter, array_keys($extrafields->attribute_label));
 
 
@@ -215,7 +220,7 @@ if (! empty($sortorder))
 if (! empty($page))
 	print '<input type="hidden" name="page" value="' . $page . '"/>';
 
-	print_barre_liste($langs->trans("AgfMenuCat"), $page, $_SERVER ['PHP_SELF'], '&arch=' . $arch, $sortfield, $sortorder, '', $resql,$resql,'title_generic.png', 0, '', '', $limit);
+	print_barre_liste($langs->trans("AgfMenuCat"), $page, $_SERVER ['PHP_SELF'], '&arch=' . $arch, $sortfield, $sortorder, '', $resql,$nbtotalofrecords,'title_generic.png', 0, '', '', $limit);
 
 
 $varpage=empty($contextpage)?$_SERVER["PHP_SELF"]:$contextpage;
