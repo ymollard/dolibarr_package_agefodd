8f2fe1d5ca1e2beba94a2886fee852371e5efe50
florian HENRY <florian.henry@atm-consulting.fr>
2018-01-10 12:18:25 +0100

    fix column siez

--- sql/update_3.0.15-3.0.18.sql
+++ sql/update_3.0.15-3.0.18.sql
@@ -1,2 +1,3 @@
 ALTER TABLE llx_agefodd_training_admlevel ADD COLUMN delais_alerte_end integer AFTER delais_alerte;
 ALTER TABLE llx_agefodd_session_admlevel ADD COLUMN delais_alerte_end integer AFTER delais_alerte;
+ALTER TABLE llx_agefodd_session_adminsitu MODIFY intitule varchar(150) default NULL;
