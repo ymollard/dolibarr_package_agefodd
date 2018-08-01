26fe1ee4c1961fa4995bf96219bf3ca1c2b35f3f
florian HENRY <florian.henry@atm-consulting.fr>
2018-08-01 14:24:25 +0200

    fix version and warining message

--- core/modules/modAgefodd.class.php
+++ core/modules/modAgefodd.class.php
@@ -58,7 +58,7 @@ class modAgefodd extends DolibarrModules
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Trainning Management Assistant Module";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '3.5';
+		$this->version = '4.0';
 
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_' . strtoupper($this->name);
@@ -2509,7 +2509,7 @@ class modAgefodd extends DolibarrModules
 						if($last_version_install <='3.2' && $data['toversion']>='3.3') {
 							$this->update_refsession();
 						}
-								
+
 								if ($result <= 0)
 									$error ++;
 							}
