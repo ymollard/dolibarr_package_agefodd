3d8019a296b2e77f1a349f6add170d8736c84fb3
atm-greg <gregory.blemand@atm-consulting.fr>
2018-08-31 15:42:58 +0200

    fix bad session number on lists (nb sessions instead of nb records)

--- class/agsession.class.php
+++ class/agsession.class.php
@@ -2360,6 +2360,9 @@ class Agsession extends CommonObject
 			$i = 0;
 
 			if ($num) {
+			    $nbsess = 0;
+			    $Tsessid = array();
+			    
 				while ( $i < $num ) {
 					$obj = $this->db->fetch_object($resql);
 
@@ -2424,13 +2427,18 @@ class Agsession extends CommonObject
 					{
 						$line->array_options['options_'.$key] = $obj->{$key};
 					}
+					
+					if (!in_array($line->rowid, $Tsessid)) {
+					    $Tsessid[] = $line->rowid;
+					    $nbsess++;
+					}
 
 					$this->lines[$i] = $line;
 					$i ++;
 				}
 			}
 			$this->db->free($resql);
-			return $num;
+			return $nbsess;
 		} else {
 			$this->error = "Error " . $this->db->lasterror();
 			dol_syslog(get_class($this) . "::fetch_all " . $this->error, LOG_ERR);
