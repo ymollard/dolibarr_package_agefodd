7c970c70c6686c10c084e1dfa905b138a1d0c7a4
florian HENRY <florian.henry@atm-consulting.fr>
2018-10-15 15:26:23 +0200

    fix lgenth customer name

--- core/modules/agefodd/pdf/pdf_courrier.modules.php
+++ core/modules/agefodd/pdf/pdf_courrier.modules.php
@@ -282,7 +282,7 @@ class pdf_courrier extends ModelePDFAgefodd {
 				$pdf->SetXY($posX + 20, $posY + 3);
 				$pdf->SetFont(pdf_getPDFFont($outputlangs), 'B', 12);
 				$this->str = $agf_soc->nom;
-				$pdf->MultiCell(86, 4, $outputlangs->convToOutputCharset($this->str), 0, 'L');
+				$pdf->MultiCell(80, 4, $outputlangs->convToOutputCharset($this->str), 0, 'L');
 
 				// Show recipient information
 
