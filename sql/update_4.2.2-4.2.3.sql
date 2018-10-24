6c252069c23501377c044169721204ea465d83a6
florian HENRY <florian.henry@atm-consulting.fr>
2018-10-24 15:28:21 +0200

    fix migration SQL script from 4.2.2 to 4.2.3

--- sql/update_4.2.2-4.2.3.sql
+++ sql/update_4.2.2-4.2.3.sql
@@ -1,3 +1,5 @@
+ALTER TABLE llx_agefodd_session_adminsitu ADD COLUMN delais_alerte_end integer NOT NULL DEFAULT 0 AFTER delais_alerte;
+
 UPDATE llx_agefodd_session_admlevel SET delais_alerte_end=0 WHERE delais_alerte_end IS NULL;
 
 UPDATE llx_agefodd_training_admlevel SET delais_alerte_end=0 WHERE delais_alerte_end IS NULL;
@@ -10,6 +12,5 @@ ALTER TABLE llx_agefodd_session_admlevel MODIFY COLUMN delais_alerte_end integer
 
 UPDATE llx_agefodd_training_admlevel AS dest, llx_agefodd_session_admlevel AS src SET dest.delais_alerte_end = src.delais_alerte_end WHERE dest.fk_agefodd_training_admlevel = src.rowid;
 
-UPDATE llx_agefodd_training_admlevel AS dest, llx_agefodd_session_admlevel AS src SET dest.delais_alerte_end = src.delais_alerte_end WHERE dest.fk_agefodd_training_admlevel = src.rowid;
+UPDATE llx_agefodd_session_adminsitu AS dest, llx_agefodd_training_admlevel AS src SET dest.delais_alerte_end = src.delais_alerte_end WHERE dest.fk_agefodd_session_admlevel = src.rowid;
 
-ALTER TABLE llx_agefodd_session_adminsitu ADD COLUMN delais_alerte_end integer NOT NULL DEFAULT 0 ALTER delais_alerte;
