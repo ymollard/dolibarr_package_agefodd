3206a3d0b3fe34f45b328883dace940cdddfe10d
florian HENRY <florian.henry@atm-consulting.fr>
2018-12-05 17:19:14 +0100

    add paramters into hook

--- session/document_files.php
+++ session/document_files.php
@@ -79,7 +79,7 @@ if ($result < 0) {
 	$upload_dir = $conf->agefodd->dir_output . "/" . $object->id;
 }
 
-$parameters = array('id'=>$id);
+$parameters = array('id'=>$id, 'upload_dir'=>$upload_dir);
 $reshook = $hookmanager->executeHooks('doActions', $parameters, $object, $action); // Note that $action and $object may have been modified by some hooks
 if ($reshook < 0)
 	setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
