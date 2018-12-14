1f09a7b701fe21c08cd925f0aadbd30e73f2e835
atm-greg <gregory.blemand@atm-consulting.fr>
2018-12-14 11:13:45 +0100

    génération du fichier

--- report/report_calendar_by_customer.php
+++ report/report_calendar_by_customer.php
@@ -91,34 +91,39 @@ if (! empty($search_soc)) {
 if ($action == 'builddoc') {
 
 	if (count($filter)>0) {
-	    
-    	$outputlangs = $langs;
-    	$newlang = $lang_id;
-    	if ($conf->global->MAIN_MULTILANGS && empty($newlang))
-    		$newlang = $object->client->default_lang;
-    	if (! empty($newlang)) {
-    		$outputlangs = new Translate("", $conf);
-    		$outputlangs->setDefaultLang($newlang);
-    	}
-    
-    	$outputlangs->load('agefodd@agefodd');
-    
-    	$report_calendar_by_cust = new ReportCalendarByCustomer($db, $outputlangs);
-    
-    	if (empty($session_dt_end)) $filter['sesscal.date_session']['end'] = dol_now();
-    	
-    	$file_sub_title=$report_calendar_by_cust->getSubTitlFileName($filter);
-    	$report_calendar_by_cust->file = $upload_dir . 'reportcalendarbycust-' . $file_sub_title . '.xlsx';
-    
-    	$result = $report_calendar_by_cust->write_file($filter);
-    	if ($result < 0) {
-    		setEventMessage($report_calendar_by_cust->error, 'errors');
-    	} elseif ($result == 0) {
-    		setEventMessage($langs->trans("NoData"), 'warnings');
-    	} else {
-    		setEventMessage($langs->trans("FileSuccessfullyBuilt"));
-    	}
-    	
+	    if (empty($filter['so.rowid']))
+	    {
+	        setEventMessage($langs->trans("AgfRptSelectACustomer"), 'errors');
+	    }
+	    else
+	    {
+        	$outputlangs = $langs;
+        	$newlang = $lang_id;
+        	if ($conf->global->MAIN_MULTILANGS && empty($newlang))
+        		$newlang = $object->client->default_lang;
+        	if (! empty($newlang)) {
+        		$outputlangs = new Translate("", $conf);
+        		$outputlangs->setDefaultLang($newlang);
+        	}
+        
+        	$outputlangs->load('agefodd@agefodd');
+        
+        	$report_calendar_by_cust = new ReportCalendarByCustomer($db, $outputlangs);
+        
+        	if (empty($session_dt_end)) $filter['sesscal.date_session']['end'] = dol_now();
+        	
+        	$file_sub_title=$report_calendar_by_cust->getSubTitlFileName($filter);
+        	$report_calendar_by_cust->file = $upload_dir . 'reportcalendarbycust-' . $file_sub_title . '.xlsx';
+        
+        	$result = $report_calendar_by_cust->write_file($filter);
+        	if ($result < 0) {
+        		setEventMessage($report_calendar_by_cust->error, 'errors');
+        	} elseif ($result == 0) {
+        		setEventMessage($langs->trans("NoData"), 'warnings');
+        	} else {
+        		setEventMessage($langs->trans("FileSuccessfullyBuilt"));
+        	}
+	    }
 	} else {
 		setEventMessage($langs->trans("AgfRptSelectAtLeastOneCriteria"), 'errors');
 	}
@@ -145,21 +150,16 @@ print '<form method="post" action="' . $_SERVER['PHP_SELF'] . '" name="search_fo
 print '<table class="border" width="100%">';
 
 print '<tr>';
+print '<td class="fieldrequired">' . $langs->trans('Company') . '</td>';
+print '<td>' . $form->select_company($search_soc, 'search_soc', '', 1) . '</td>';
+print '</tr>';
+
+print '<tr>';
 print '<td>' . $langs->trans('AgfSessionDetail') . '</td>';
 print '<td>';
 print $langs->trans("AgfDateDebut") . ':'.$form->select_date($session_dt_st, 'session_dt_st',0,0,1,'search_form',1,1,1);
 print ' ' . $langs->trans("AgfDateFin") . ' ' . $form->select_date($session_dt_end, 'session_dt_end',0,0,1,'search_form',1,1,1);
 print '</td>';
-print '</tr>';
-
-// print '<tr>';
-// print '<td>' . $langs->trans('Company') . '</td>';
-// print '<td>' . $form->select_company($search_soc, 'search_soc', $filter, 1) . '<input type="text" class="flat" name="search_soc" value="' . $search_soc . '" size="20"></td>';
-// print '</tr>';
-
-print '<tr>';
-print '<td>' . $langs->trans('Company') . '</td>';
-print '<td>' . $form->select_company($search_soc, 'search_soc', '', 1) . '</td>';
 print '</tr>';
 
 print '</table>' . "\n";
