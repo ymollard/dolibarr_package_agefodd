a74f8d326aaae5d48e14ce7f7993575e1354f115
Alexis serveur clone <alexis@atm-consulting.fr>
2019-04-15 17:44:33 +0200

    NEW (got) : tk9456 : Historisation de l'envoi de l'attestation de présence par mail

--- sql/data_2.0.sql
+++ sql/data_2.0.sql
@@ -69,6 +69,7 @@ INSERT INTO llx_c_actioncomm (id, code, type, libelle, module, active, todo, pos
 INSERT INTO llx_c_actioncomm (id, code, type, libelle, module, active, todo, position) VALUES (1030004, 'AC_AGF_PEDAG', 'agefodd', 'Send Fiche pédagogique by mail', 'agefodd', 1, NULL, 40);
 INSERT INTO llx_c_actioncomm (id, code, type, libelle, module, active, todo, position) VALUES (1030005, 'AC_AGF_PRES', 'agefodd', 'Send Fiche présence by mail', 'agefodd', 1, NULL, 50);
 INSERT INTO llx_c_actioncomm (id, code, type, libelle, module, active, todo, position) VALUES (1030006, 'AC_AGF_ATTES', 'agefodd', 'Send attestation by mail', 'agefodd', 1, NULL, 60);
+INSERT INTO llx_c_actioncomm (id, code, type, libelle, module, active, todo, position) VALUES (1030006, 'AC_AGF_ATTEP', 'agefodd', 'Send attestation présence by mail', 'agefodd', 1, NULL, 60);
 INSERT INTO llx_c_actioncomm (id, code, type, libelle, module, active, todo, position) VALUES (1030007, 'AC_AGF_CLOT', 'agefodd', 'Send dossier cloture by mail', 'agefodd', 1, NULL, 70);
 INSERT INTO llx_c_actioncomm (id, code, type, libelle, module, active, todo, position) VALUES (1030008, 'AC_AGF_CONSE', 'agefodd', 'Send Advise document by mail', 'agefodd', 1, NULL, 80);
 INSERT INTO llx_c_actioncomm (id, code, type, libelle, module, active, todo, position) VALUES (1030009, 'AC_AGF_ACCUE', 'agefodd', 'Send welcome document by mail', 'agefodd', 1, NULL, 90);
---
+++ sql/update_3.3.8-3.3.9.sql
@@ -0,0 +1,1 @@
+INSERT INTO llx_c_actioncomm (id, code, type, libelle, module, active, todo, position) VALUES (1030006, 'AC_AGF_ATTEP', 'agefodd', 'Send attestation présence by mail', 'agefodd', 1, NULL, 60);
