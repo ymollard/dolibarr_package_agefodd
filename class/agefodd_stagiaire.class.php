0afa92258f4dd1e1eb0a6920b03c15a09c78eef2
John BOTELLA <john.botella@atm-consulting.fr>
2019-05-14 15:48:50 +0200

    Add triggers

--- class/agefodd_stagiaire.class.php
+++ class/agefodd_stagiaire.class.php
@@ -160,15 +160,12 @@ class Agefodd_stagiaire extends CommonObject {
 
 			$this->id = $this->db->last_insert_id(MAIN_DB_PREFIX . "agefodd_stagiaire");
 			if (! $notrigger) {
-				// Uncomment this and change MYOBJECT to your own tag if you
-				// want this action call a trigger.
-
-				// // Call triggers
-				// include_once(DOL_DOCUMENT_ROOT . "/interfaces.class.php");
-				// $interface=new Interfaces($this->db);
-				// $result=$interface->run_triggers('MYOBJECT_CREATE',$this,$user,$langs,$conf);
-				// if ($result < 0) { $error++; $this->errors=$interface->errors; }
-				// // End call triggers
+				 // Call triggers
+				 include_once(DOL_DOCUMENT_ROOT . "/interfaces.class.php");
+				 $interface=new Interfaces($this->db);
+				 $result=$interface->run_triggers('AGEFODD_STAGIAIRE_CREATE',$this,$user,$langs,$conf);
+				 if ($result < 0) { $error++; $this->errors=$interface->errors; }
+				 // End call triggers
 			}
 
 			$result = $this->insertExtraFields();
@@ -577,15 +574,12 @@ class Agefodd_stagiaire extends CommonObject {
 		}
 		if (! $error) {
 			if (! $notrigger) {
-				// Uncomment this and change MYOBJECT to your own tag if you
-				// want this action call a trigger.
-
-				// // Call triggers
-				// include_once(DOL_DOCUMENT_ROOT . "/interfaces.class.php");
-				// $interface=new Interfaces($this->db);
-				// $result=$interface->run_triggers('MYOBJECT_MODIFY',$this,$user,$langs,$conf);
-				// if ($result < 0) { $error++; $this->errors=$interface->errors; }
-				// // End call triggers
+				 // Call triggers
+				 include_once(DOL_DOCUMENT_ROOT . "/interfaces.class.php");
+				 $interface=new Interfaces($this->db);
+				 $result=$interface->run_triggers('AGEFODD_STAGIAIRE_MODIFY',$this,$user,$langs,$conf);
+				 if ($result < 0) { $error++; $this->errors=$interface->errors; }
+				 // End call triggers
 			}
 
 			$result = $this->insertExtraFields();
