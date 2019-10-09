fda192db07c4b83458ae459e520eb208af395f5a
John Botella <john.botella@atm-consulting.fr>
2019-10-09 12:12:29 +0200

    Remove console log

--- lib/agf_externalaccess.lib.php
+++ lib/agf_externalaccess.lib.php
@@ -1910,7 +1910,7 @@ function getPageViewSessionCardCalendrierFormateurExternalAccess($agsession, $tr
                 var startDate = new Date(0, 0, 0, start[0], start[1], 0);
                 var endDate = new Date(0, 0, 0, end[0], end[1], 0);
                 var diff = endDate.getTime() - startDate.getTime();
-                console.log(diff);
+                // console.log(diff);
                 if(outputFormated){
 					var hours = Math.floor(diff / 1000 / 60 / 60);
 					diff -= hours * 1000 * 60 * 60;
