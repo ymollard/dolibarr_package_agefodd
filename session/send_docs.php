9970b34535cbab4b79c940869657a9571c84b1e7
BlackMirror <blackmirror@exemple.com>
2019-10-14 11:28:30 +0200

    [greg] fix TK10242 saut de lignes dans emails

--- session/send_docs.php
+++ session/send_docs.php
@@ -1380,6 +1380,7 @@ if (! empty($id)) {
 				$formmail->withbody .= $langs->transnoentities('AgfPDFCourrierConv3') . "\n\n";
 				$formmail->withbody .= $langs->transnoentities('AgfPDFCourrierAcceuil11') . "\n\n";
 				$formmail->withbody .= $langs->transnoentities('AgfPDFCourrierAcceuil13');
+				$formmail->withbody = nl2br($formmail->withbody);
 
 				$formmail->param['models'] = 'convention';
 				$formmail->param['pre_action'] = 'presend_convention';
