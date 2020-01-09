245fbd30141418e4f5a2420ef94285b5a5581025
John Botella <john.botella@atm-consulting.fr>
2020-01-09 11:43:49 +0100

    Fix group by

--- class/agsession.class.php
+++ class/agsession.class.php
@@ -2678,7 +2678,7 @@ class Agsession extends CommonObject
 				}
 			}
 		}
-		$sql .= " GROUP BY s.rowid,soemployer.nom,dictstatusba.intitule, s.fk_soc, s.fk_session_place, s.type_session, s.dated, s.datef,  s.status, dictstatus.intitule , dictstatus.code,  s.date_res_trainer, s.color, s.force_nb_stagiaire, s.nb_stagiaire,s.notes,";
+		$sql .= " GROUP BY s.rowid,soemployer.nom,dictstatusba.intitule,dictstatusba.code, s.fk_soc, s.fk_session_place, s.type_session, s.dated, s.datef,  s.status, dictstatus.intitule , dictstatus.code,  s.date_res_trainer, s.color, s.force_nb_stagiaire, s.nb_stagiaire,s.notes,";
 		$sql .= " p.ref_interne, c.intitule, c.ref,c.ref_interne, so.nom, f.rowid,socp.rowid,sa.archive,sorequester.nom,c.color,agefoddcontact.fk_socpeople, sale.fk_user_com";
 		foreach ( $array_options_keys as $key ) {
 			$sql.= ',ef.'.$key;
