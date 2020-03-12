2363b717b1f397a1f2614e7698a43d86580d3a04
atm-lena <lena@atm-consulting.fr>
2020-03-12 09:43:16 +0100

    T2014/2015 - Conf "Hide Legal Meaning" in pdf presence landscape empty/landscape by month, clean code

--- core/modules/agefodd/pdf/pdf_fiche_presence_landscape_empty.modules.php
+++ core/modules/agefodd/pdf/pdf_fiche_presence_landscape_empty.modules.php
@@ -317,9 +317,10 @@ class pdf_fiche_presence_landscape_empty extends pdf_fiche_presence
             $pdf->SetXY($posX + $this->trainer_widthcol1, $posY + 3);
             $pdf->SetFont(pdf_getPDFFont($outputlangs), 'I', 7);
             $showAdditionalText = empty($conf->global->AGF_FICHE_PRES_HIDE_LEGAL_MEANING_BELOW_SIGNATURE_HEADER);
-            if ($showAdditionalText) $str = $outputlangs->transnoentities('AgfPDFFichePres13');
-            else $str = '';
-            $pdf->Cell(0, 5, $outputlangs->convToOutputCharset($str), 'LR', 2, "C", 0);
+            if ($showAdditionalText) {
+                $str = $outputlangs->transnoentities('AgfPDFFichePres13');
+                $pdf->Cell(0, 5, $outputlangs->convToOutputCharset($str), 'LR', 2, "C", 0);
+            }
             $posY += $this->h_ligne;
 
             // Date
@@ -411,9 +412,10 @@ class pdf_fiche_presence_landscape_empty extends pdf_fiche_presence
         $pdf->SetXY($posX + $this->trainee_widthcol1 + $this->trainee_widthcol2, $posY + 3);
         $pdf->SetFont(pdf_getPDFFont($outputlangs), 'I', 7);
         $showAdditionalText = empty($conf->global->AGF_FICHE_PRES_HIDE_LEGAL_MEANING_BELOW_SIGNATURE_HEADER);
-        if ($showAdditionalText) $str = $outputlangs->transnoentities('AgfPDFFichePres19');
-        else $str = '';
-        $pdf->Cell(0, 5, $outputlangs->convToOutputCharset($str), 'LR', 2, "C", 0);
+        if ($showAdditionalText) {
+            $str = $outputlangs->transnoentities('AgfPDFFichePres19');
+            $pdf->Cell(0, 5, $outputlangs->convToOutputCharset($str), 'LR', 2, "C", 0);
+        }
         $posY += $this->h_ligne;
 
         // Date
