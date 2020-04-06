8a4afe5269cb2a661fd67cdd314acd5e1b4e1603
atm-lena <lena@atm-consulting.fr>
2020-04-06 16:59:45 +0200

    T2058 - Ajout des lignes et des colonnes du tableau de plannification

--- session/planningpertrainee.php
+++ session/planningpertrainee.php
@@ -85,12 +85,89 @@ if ($id)
 }
 
 //Tableau pour chaque participant de la session
-$test = new Agefodd_session_stagiaire($db);
-$res = $test->fetch_stagiaire_per_session($id);
+$session_trainee = new Agefodd_session_stagiaire($db);
+$res = $session_trainee->fetch_stagiaire_per_session($id);
 
-foreach($test->lines as $line)
+if($res > 0)
 {
-    print load_fiche_titre($langs->trans('AgfTraineePlanification'), '', '');
+
+    foreach ($session_trainee->lines as $line)
+    {
+
+        //Tableau de toutes les heures plannifiées du participant
+        $TLinesTraineePlanification = array();
+
+        $sql = "SELECT rowid, fk_calendrier_type, heurep ";
+        $sql.= "FROM ".MAIN_DB_PREFIX."agefodd_session_stagiaire_planification ";
+        $sql.= "WHERE fk_agefodd_session = '" . $id . "' AND fk_agefodd_session_stagiaire = '".$line->id."'";
+        $resql = $db->query($sql);
+
+        if($resql)
+        {
+            while ($obj = $db->fetch_object($resql))
+            {
+                $TLinesTraineePlanification[] = $obj;
+            }
+        }
+
+        print '<div class="" id="formdateall">';
+        print '<form name="obj_update" action="'.$_SERVER['PHP_SELF'].'?action=edit&id='.$id.'"  method="POST">'."\n";
+        print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">'."\n";
+        print '<input type="hidden" name="action" value="edit">'."\n";
+        print '<input type="hidden" name="sessid" value="'.$agf->id.'">'."\n";
+        print load_fiche_titre($langs->trans('AgfTraineePlanification'), '', '', 0, 0, '', $massactionbutton);
+        print '<table class="noborder period" width="100%" id="period">';
+
+        print '<tr class="liste_titre">';
+        print '<th width="15%" class="liste_titre">'.$langs->trans('').'</th>';
+        print '<th width="35%" class="liste_titre">'.$langs->trans('AgfHoursP').'</th>';
+        print '<th class="text-center" >'.$langs->trans('AgfHoursR').'</th>';
+        print '<th class="liste_titre">'.$langs->trans('AgfHoursRest').'</th>';
+        print '</tr>';
+
+
+        print '<tr>';
+        print '<td>Total</td>';
+        print '<td>'.$agf->duree_session.'</td>';
+        print '<td></td>';
+        print '<td></td>';
+
+        print '</tr>';
+
+
+
+        foreach($TLinesTraineePlanification as $lineP)
+        {
+            print '<tr>';
+
+            //Modalité
+            $sql = "SELECT";
+            $sql .= " label ";
+            $sql .= " FROM ".MAIN_DB_PREFIX."c_agefodd_session_calendrier_type";
+            $sql .= " WHERE rowid = '".$lineP->fk_calendrier_type . "'";
+            $resql = $db->query($sql);
+
+            if($resql)
+            {
+                $obj = $db->fetch_object($resql);
+
+                print '<td>'.$obj->label.'</td>';
+            }
+
+            //Heure saisie prévue
+            print '<td>'.$lineP->heurep.'</td>';
+
+            //Heure réalisées
+            print '<td>'.$lineP->heurep.'</td>';
+
+            //Heure restante
+
+
+            print '</tr>';
+
+        }
+
+    }
 
 }
 
