3dc65f99d0a616d97a391aab72e108d60b67ecf5
jpb <jean-pascal.boudet@atm-consulting.fr>
2021-02-18 15:47:23 +0100

    change version module

--- core/modules/modAgefodd.class.php
+++ core/modules/modAgefodd.class.php
@@ -58,7 +58,7 @@ class modAgefodd extends DolibarrModules
 		// Module description, used if translation string 'ModuleXXXDesc' not found (where XXX is value of numeric property 'numero' of module)
 		$this->description = "Trainning Management Assistant Module";
 		// Possible values for version are: 'development', 'experimental', 'dolibarr' or version
-		$this->version = '4.12.8';
+		$this->version = '4.12.9';
 
 		// Key used in llx_const table to save module status enabled/disabled (where MYMODULE is value of property name of module in uppercase)
 		$this->const_name = 'MAIN_MODULE_' . strtoupper($this->name);
